0023fe7b1195ff58a762fb5c9d3ae789f1e09081
Chen Harel <chen.harel@takipi.com>
2015-01-13 16:18:27 +0200

    Record

---
+++ src/main/java/com/takipi/oss/storage/fs/Record.java
@@ -0,0 +1,34 @@
+package com.takipi.oss.storage.fs;
+
+public class Record {
+    private final String serviceId;
+    private final String type;
+    private final String key;
+
+    public Record(String serviceId, String type, String key) {
+        this.serviceId = serviceId;
+        this.type = type;
+        this.key = key;
+    }
+    
+    public static Record newRecord(String serviceId, String type, String key) {
+        return new Record(serviceId, type, key);
+    }
+
+    public String getServiceId() {
+        return serviceId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    @Override
+    public String toString() {
+        return "service: " + serviceId + ". type: " + type + ". key: " + key + ".";
+    }
+}
