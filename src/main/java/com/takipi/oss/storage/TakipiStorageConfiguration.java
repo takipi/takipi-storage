6471e311268ddc1c2368a8690aa5f0baac062b71
shimonmagal <shimon.magal@takipi.com>
2017-08-02 08:20:31 +0300

    add support for not passing credentials for s3

--- src/main/java/com/takipi/oss/storage/TakipiStorageConfiguration.java
+++ src/main/java/com/takipi/oss/storage/TakipiStorageConfiguration.java
@@ -62,10 +62,9 @@ public class TakipiStorageConfiguration extends Configuration {
         private Credentials credentials;
 
         public static class Credentials {
-            @NotEmpty
+
             private String accessKey;
 
-            @NotEmpty
             private String secretKey;
 
             @JsonProperty
