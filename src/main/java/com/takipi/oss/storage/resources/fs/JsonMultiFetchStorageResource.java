8750dd922d9355a61752bdd0e74f738129cf56f3
mervyn2017 <mervynk@gmail.com>
2017-10-02 18:47:55 +0300

    base multifetcher type on file system type

--- src/main/java/com/takipi/oss/storage/resources/fs/JsonMultiFetchStorageResource.java
+++ src/main/java/com/takipi/oss/storage/resources/fs/JsonMultiFetchStorageResource.java
@@ -5,6 +5,7 @@ import com.takipi.oss.storage.data.fetch.MultiFetchRequest;
 import com.takipi.oss.storage.data.fetch.MultiFetchResponse;
 import com.takipi.oss.storage.fs.Record;
 import com.takipi.oss.storage.fs.api.Filesystem;
+import com.takipi.oss.storage.fs.s3.S3Filesystem;
 import com.takipi.oss.storage.resources.fs.fetcher.*;
 
 import javax.ws.rs.Consumes;
@@ -25,21 +26,26 @@ public class JsonMultiFetchStorageResource {
     //private static final Cache cache = new DummyCache();
     
     private final Filesystem<Record> filesystem;
-    
-    private final MultiFetcher multiFetcher = new ConcurrentMultiFetcher();
-    //private final MultiFetcher multiFetcher = new SequentialMultiFetcher();
+    private final MultiFetcher multiFetcher;
     
     public JsonMultiFetchStorageResource(Filesystem<Record> filesystem) {
+
         this.filesystem = filesystem;
+
+        if (filesystem instanceof S3Filesystem) {
+            multiFetcher = new ConcurrentMultiFetcher();
+        }
+        else {
+            multiFetcher = new SequentialMultiFetcher();
+        }
     }
     
     @POST
     @Timed
     public Response post(MultiFetchRequest request) {
+
         try {
-    
             MultiFetchResponse response = multiFetcher.loadData(request, filesystem, cache);
-            
             return Response.ok(response).build();
         }
         catch (Exception e) {
