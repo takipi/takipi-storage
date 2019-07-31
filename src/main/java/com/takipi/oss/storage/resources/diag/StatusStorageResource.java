5d4f32a16c946ef15c0e323eab3d41682682cc31
nivstein <niv@takipi.com>
2019-07-31 05:41:00 +0300

    Only allow one concurrent collection; return cached version otherwise

--- src/main/java/com/takipi/oss/storage/resources/diag/StatusStorageResource.java
+++ src/main/java/com/takipi/oss/storage/resources/diag/StatusStorageResource.java
@@ -1,5 +1,7 @@
 package com.takipi.oss.storage.resources.diag;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -18,20 +20,42 @@ import com.takipi.oss.storage.helper.MachineStatusCollector;
 public class StatusStorageResource {
 	private final String basePath;
 	
+	private final Lock lock;
+	private volatile MachineStatus lastMachineStatus;
+	
 	public StatusStorageResource(TakipiStorageConfiguration configuration) {
 		this.basePath = configuration.getFolderPath();
+		
+		this.lock = new ReentrantLock();
+		this.lastMachineStatus = null;
 	}
 	
 	@POST
 	@Timed
 	public Response post() {
-		MachineStatusCollector collector = new MachineStatusCollector(basePath);
-		MachineStatus machineStatus = collector.collect();
+		MachineStatus machineStatus = getMachineStatus();
 		
 		if (machineStatus != null) {
 			return Response.ok(machineStatus).build();
 		} else {
 			return Response.serverError().entity("Failed retrieving System Status").build();
 		}
+	}
+	
+	private MachineStatus getMachineStatus() {
+		if (lock.tryLock()) {
+			try {
+				MachineStatusCollector collector = new MachineStatusCollector(basePath);
+				MachineStatus machineStatus = collector.collect();
+				
+				if (machineStatus != null) {
+					lastMachineStatus = machineStatus;
+				}
+			} finally {
+				lock.unlock();
+			}
+		}
+		
+		return lastMachineStatus;
 	}
 }
